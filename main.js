ff4cd6a62c4f2dcb041e7e14d567e3c5c3bff3b9
Ilan bit <ilanbit23@gmail.com>
2020-10-21 23:44:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    var navContainer = document.querySelector('.nav-container');
+    var nav = document.querySelector('.nav');
+    nav.classList.toggle('show');
+    navContainer.classList.toggle('show');
+}
